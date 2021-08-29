18017dc1edb92908cbd46617fa5f4d82166d4151
Bri <bhilliker@gmail.com>
2021-08-28 22:17:31 -0500

    updated associations to fix bugs

--- models/index.js
+++ models/index.js
@@ -5,27 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'category_id'
-});
+Product.belongsTo(Category);
 
 // Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'category_id'
-});
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_id',
-  foreignKey: 'tag_id'
+  as: 'product_tags',
+  foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'tag_id',
-  foreignKey: 'category_id'
+  as: 'product_tags',
+  foreignKey: 'tag_id'
 });
 
 
